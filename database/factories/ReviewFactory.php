cd80799e13364c67b756e0e9db5234ada9e40692
RonnieHijazy <ronnyhijazy@gmail.com>
2020-07-08 15:23:09 +0300

    Generte Seeding for Product and Review

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,11 +2,29 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model\Review;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Model\Review;
+use App\Model\Product;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+		'product_id' => function(){
+			return Product::all()->random();
+		},
+        'customer' => $faker->word,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0 , 5),
     ];
 });
