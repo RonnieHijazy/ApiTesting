62ed4ecff8f1c25a6aacac895b9269949c31d7a5
RonnieHijazy <ronnyhijazy@gmail.com>
2020-07-08 16:53:49 +0300

    RelationShip Created!

--- database/migrations/2020_07_07_172529_create_reviews_table.php
+++ database/migrations/2020_07_07_172529_create_reviews_table.php
@@ -16,9 +16,9 @@ class CreateReviewsTable extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
 
-			$table->integer('product_id')->unsigned();
-            $table->index('product_id');
-
+			$table->unsignedBigInteger('product_id');
+			$table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+			
 			$table->string('customer');
 			$table->text('review');
 			$table->integer('star');
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-		/*factory(App\Model\Product::class , 50)->create();
-		factory(App\Model\Review::class , 300)->create();*/
 		
 		//factory(App\User::class , 10)->create();
 		//factory(App\Model\Product::class , 50)->create();
